1f1ae47f2157b38fad2d5d711beca1fdc3e9681e
Mike <mike.s3crypto@gmail.com>
2023-10-25 22:39:01 +0100

    Added Actions
    Actions for Adding Todos, Toggling Todos, and updating a filter

--- ToDoTask/ClientApp/src/actions/todosActions.js
+++ ToDoTask/ClientApp/src/actions/todosActions.js
@@ -1,17 +1,41 @@
 ﻿// Action types
 export const SET_TODOS = 'SET_TODOS';
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_FILTER = "SET_FILTER";
+
 
 // Action creators
-export const setTodos = (todos) => ({
-    type: SET_TODOS,
-    payload: todos
-});
-
 export const getTodos = () => {
     return async dispatch => {
-        // Fetch data from the backend API (change the endpoint if necessary)
         const response = await fetch('/Todos');
         const data = await response.json();
         dispatch(setTodos(data));
     };
 };
+
+export const setTodos = (todos) => ({
+    type: SET_TODOS,
+    payload: todos
+});
+
+let nextTodoId = 0;
+
+export const addTodo = (content) => ({
+    type: ADD_TODO,
+    payload: {
+        id: ++nextTodoId,
+        content
+    }
+});
+
+export const toggleTodo = (id) => ({
+    type: TOGGLE_TODO,
+    payload: { id }
+});
+
+export const setFilter = (filter) => ({
+    type: SET_FILTER,
+    payload: { filter }
+});
+
