bac4ff95cbad3f13488a54d20d00bc053aa53e93
Mike <mike.s3crypto@gmail.com>
2023-10-25 22:57:05 +0100

    Removed unused components

--- ToDoTask/ClientApp/src/actions/todosActions.js
+++ ToDoTask/ClientApp/src/actions/todosActions.js
@@ -2,8 +2,6 @@
 export const SET_TODOS = 'SET_TODOS';
 export const ADD_TODO = "ADD_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
-export const SET_FILTER = "SET_FILTER";
-
 
 // Action creators
 export const getTodos = () => {
@@ -33,9 +31,3 @@ export const toggleTodo = (id) => ({
     type: TOGGLE_TODO,
     payload: { id }
 });
-
-export const setFilter = (filter) => ({
-    type: SET_FILTER,
-    payload: { filter }
-});
-
